8246b20cfe1554595c60d57bf6cf04e49a701d5c
Nikitamt <ntarasov0326@icloud.com>
2023-04-25 11:00:16 +0300

    need a city

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,4 +1,7 @@
-export const handleWeatherByGeolocation = () => {
+import { getWeatherData } from "./api.js";
+import { resetWeatherContent } from "./helper.js";
+
+export const handleWeatherByGeolocation = async () => {
     const options = {
         enableHighAccuracy: true,       /* для более точного определения местоположения */
         timeout: 5000,      /* устанавливает временной интервал обновления геолокаци */
@@ -9,9 +12,12 @@ export const handleWeatherByGeolocation = () => {
         const crd = pos.coords;
 
         const response = await fetch(`https://api.geoapify.com/v1/geocode/reverse?lat=${crd.latitude}&lon=${crd.longitude}&apiKey=00c7dbe2eca64697bb9c9e11ce403367`);
-
         const result = await response.json();
-        console.log(result);
+
+        const weather = await getWeatherData(result.features[0].properties.city);
+        
+        resetWeatherContent(result.features[0].properties.city, weather);
+        
     }
 
     const error = (err) => {
